fa28a3815b74cb33d20650ca1f5d66f607ee3892
Gustav Rasmussen <gcr84@hotmail.com>
2026-06-11 20:07:05 +0200

    Add vim-tmux-navigator plugin for seamless navigation

---
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,12 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  lazy = false,
+  config = function()
+    -- Keymaps:
+    -- C-h - navigate left
+    -- C-j - navigate down
+    -- C-k - navigate up
+    -- C-l - navigate right
+    -- Works seamlessly with tmux splits
+  end,
+}
