abc70d4e41e337ceae3c7cdc8a3b1caea7c36e8b
Gustav Rasmussen <gcr84@hotmail.com>
2026-06-11 20:08:42 +0200

    Populate plugins.lua with plugin references

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,2 +1,14 @@
 return {
+  require("plugins.alpha"),
+  require("plugins.catppuccin"),
+  require("plugins.completions"),
+  require("plugins.lsp-config"),
+  require("plugins.lualine"),
+  require("plugins.neo-tree"),
+  require("plugins.none-ls"),
+  require("plugins.telescope"),
+  require("plugins.treesitter"),
+  require("plugins.vim-surround"),
+  require("plugins.comment"),
+  require("plugins.vim-tmux-navigator"),
 }
