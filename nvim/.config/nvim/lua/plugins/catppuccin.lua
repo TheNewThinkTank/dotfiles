7cabde3f953ae10173cc0a9018a2bfa82900ef3b
Gustav Rasmussen <TheNewThinkTank@users.noreply.github.com>
2026-06-15 21:07:10 +0200

    chore: improve nvim config

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,14 +1,18 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      integrations = {
-        lualine = true,
-      }
-    })
-    vim.cmd.colorscheme "catppuccin"
-  end
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+
+    config = function()
+      require("catppuccin").setup({
+      -- integrations = {
+      --   lualine = true,
+      -- }
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end
+  }
 }
