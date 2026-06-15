7ac9769c4e68049f5d1309f5cecd6eaa21dedfc9
Gustav Rasmussen <TheNewThinkTank@users.noreply.github.com>
2026-06-15 20:46:34 +0200

    chore: improve nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,14 +1,14 @@
 return {
-  require("plugins.catppuccin"),  -- Load first with priority
-  require("plugins.alpha"),
-  require("plugins.completions"),
-  require("plugins.lsp-config"),
-  require("plugins.lualine"),
-  require("plugins.neo-tree"),
-  require("plugins.none-ls"),
-  require("plugins.telescope"),
-  require("plugins.treesitter"),
-  require("plugins.vim-surround"),
-  require("plugins.comment"),
-  require("plugins.vim-tmux-navigator"),
+  -- require("plugins.catppuccin"),  -- Load first with priority
+  -- require("plugins.alpha"),
+  -- require("plugins.completions"),
+  -- require("plugins.lsp-config"),
+  -- require("plugins.lualine"),
+  -- require("plugins.neo-tree"),
+  -- require("plugins.none-ls"),
+  -- require("plugins.telescope"),
+  -- require("plugins.treesitter"),
+  -- require("plugins.vim-surround"),
+  -- require("plugins.comment"),
+  -- require("plugins.vim-tmux-navigator"),
 }
