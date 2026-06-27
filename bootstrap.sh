6916a15515b16bfe6518372f90b67b0e9d67f0c8
Gustav Rasmussen <TheNewThinkTank@users.noreply.github.com>
2026-06-27 11:57:42 +0200

    chore: implement bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,31 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin main;
+
+function doIt() {
+  rsync --exclude ".git/" \
+    --exclude ".DS_Store" \
+    --exclude ".osx" \
+    --exclude "bootstrap.sh" \
+    --exclude "README.md" \
+    --exclude "LICENSE" \
+    -avh --no-perms . ~;
+  # source ~/.bash_profile;
+  source ~/.zsh_profile;
+  # source ~/.bashrc;
+  # source ~/.zshrc;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  doIt;
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
+fi;
+unset doIt;
+
